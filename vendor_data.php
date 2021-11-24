58aad3c3e8d63f5a477986746e06e680978d45e3
praweenaka <praweenakahemachandra@gmail.com>
2021-11-24 23:44:50 +0530

    up

---
+++ vendor_data.php
@@ -0,0 +1,131 @@
+<?php
+
+session_start();
+
+
+require_once ("connection_sql.php");
+
+header('Content-Type: text/xml');
+
+date_default_timezone_set('Asia/Colombo');
+
+if ($_POST["Command"] == "getdt") {
+    echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+
+    $ResponseXML = "";
+    $ResponseXML .= "<new>";
+
+    $sql = "SELECT VENDOR FROM invpara";
+    $result = $conn->query($sql);
+
+    $row = $result->fetch();
+    $no = $row['VENDOR'];
+    $uniq = uniqid();
+    $ResponseXML .= "<id><![CDATA[$no]]></id>";
+    $ResponseXML .= "<uniq><![CDATA[$uniq]]></uniq>";
+
+    $ResponseXML .= "</new>";
+
+    echo $ResponseXML;
+}
+
+
+if ($_POST["Command"] == "save_inv") {
+
+
+    try {
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->beginTransaction();
+
+        $sqlisalma_q = "select * from vendor where code='" . $_POST['code'] . "'";
+        $resultsalma_q = $conn->query($sqlisalma_q);
+        if ($rowsalma_q = $resultsalma_q->fetch()) {
+            $sql = "update vendor set cus_type = '" . $_POST['cus_type'] . "', name = '" . strtoupper($_POST['name']) . "',shopname =  '" . strtoupper($_POST['shopname']) . "',nic =  '" . $_POST['nic'] . "', TELE1 = '" . $_POST['land'] . "',TELE2 =  '" . $_POST['mobile'] . "', ADD1 = '" . strtoupper($_POST['address']) . "'  where code='" . $_POST['code'] . "'";
+            $result = $conn->query($sql);
+            
+            $sqllog = "insert into entry_log(refno, username, docname, trnType, stime, sdate) values ('" . trim($_POST['code']) . "', '" . $_SESSION["CURRENT_USER"] . "', 'CUSTOMER', 'Update', '" . date("Y-m-d H:i:s") . "', '" . date("Y-m-d") . "')";
+            $resultlog = $conn->query($sqllog);
+        
+            $conn->commit();
+            echo "Updated";
+        }else{
+            $sql2 = "insert into vendor(code,cus_type,NAME,shopname,nic,TELE1,TELE2,ADD1) values ('" . $_POST['code'] . "', '" . $_POST['cus_type'] . "', '" . strtoupper($_POST['name']) . "', '" . strtoupper($_POST['shopname']) . "', '" . $_POST['nic'] . "', '" . $_POST['land'] . "', '" . $_POST['mobile'] . "', '" . strtoupper($_POST['address']) . "')"; 
+            $result2 = $conn->query($sql2);
+
+            $sql = "SELECT VENDOR FROM invpara";
+            $resul = $conn->query($sql);
+            $row = $resul->fetch();
+            $no = $row['VENDOR'];
+            $no2 = $no + 1;
+            $sql = "update invpara set VENDOR = $no2 where VENDOR = $no";
+            $result = $conn->query($sql);
+            
+            $sqllog = "insert into entry_log(refno, username, docname, trnType, stime, sdate) values ('" . trim($_POST['code']) . "', '" . $_SESSION["CURRENT_USER"] . "', 'CUSTOMER', 'Save', '" . date("Y-m-d H:i:s") . "', '" . date("Y-m-d") . "')";
+            $resultlog = $conn->query($sqllog);
+            $conn->commit();
+            echo "Saved";
+        }
+
+
+    } catch (Exception $e) {
+        $conn->rollBack();
+        echo $e;
+    }
+    
+}
+
+if ($_POST["Command"] == "pass_quot") {
+    $_SESSION["custno"] = $_GET['custno'];
+
+    header('Content-Type: text/xml');
+    echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+
+    $ResponseXML = "";
+    $ResponseXML .= "<salesdetails>";
+
+    $cuscode = $_POST["custno"];
+
+    $sql = "Select * from vendor where   CODE ='" . $cuscode . "'";
+    $result = $conn->query($sql);
+
+    if ($rowM = $result->fetch()) {
+        $ResponseXML .= "<id><![CDATA[" . json_encode($rowM) .  "]]></id>";
+    }
+
+    $ResponseXML .= "<stname><![CDATA[".$_POST["stname"]."]]></stname>";
+
+    $ResponseXML .= "</salesdetails>";
+    echo $ResponseXML;
+}
+
+
+if ($_POST["Command"] == "cancel_inv") {
+
+
+    try {
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->beginTransaction();
+        
+        
+            $sql = "update vendor set cancel = '1' where  CODE='".$_POST['code']."' ";
+            $result = $conn->query($sql);
+            
+            
+         
+           $sqllog = "insert into entry_log(refno, username, docname, trnType, stime, sdate) values ('" . trim($_POST['code']) . "', '" . $_SESSION["CURRENT_USER"] . "', 'CUSTOMER', 'CANCEL', '" . date("Y-m-d H:i:s") . "', '" . date("Y-m-d") . "')";
+            $resultlog = $conn->query($sqllog);
+         
+            $conn->commit();
+            echo "Cancel";
+       
+
+        
+
+
+
+    } catch (Exception $e) {
+        $conn->rollBack();
+        echo $e;
+    }
+}
+?>
