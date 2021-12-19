1ef0cdba6fcd21d3a4fef7de447042a8a2f08e69
praweenaka <praweenakahemachandra@gmail.com>
2021-12-19 22:12:28 +0530

    up

--- gin_m.php
+++ gin_m.php
@@ -167,7 +167,7 @@
 <script src="js/common.js"></script>
 <script src="js/jquery-1.9.1.js"></script>
 <script src="js/jquery-ui.custom.js"></script>
-<script src="js/common.js"></script>
+<!-- <script src="js/common.js"></script> -->
 <script  type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
 <script  type="text/javascript" src="js/jquery-ui-1.8.14.custom.min.js"></script>
 <script>
